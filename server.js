16e4357d2bcaed256d4aa10b3ec54afaf623ac51
davidvidovic-web <mail@davidvidovic.com>
2023-04-25 04:10:58 +0200

    Getting rid of all heroku shit

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const fileupload = require("express-fileupload");
-const serverless = require("serverless-http");
-const router = express.Router();
+const port = process.env.PORT || 5030;
 const app = express();
 const { PDFDocument, StandardFonts } = require("pdf-lib");
 
-router.use(fileupload({ createParentPath: true }));
+app.use(fileupload({ createParentPath: true }));
 
-router.post("/upload", async (req, res, next) => {
+app.post("/upload", async (req, res, next) => {
   const file = req.files.pdfFile;
   const buffer = file.data;
   const pdfDoc = await PDFDocument.load(buffer);
   const pdfBytes = await pdfDoc.save({ useObjectStreams: false });
   res.set("Content-Type", "application/pdf");
-  res.json({ 'hell': "dam" });
   res.send(Buffer.from(pdfBytes));
 });
 
-app.use("./netlify/functions/api", router);
-module.exports.handler = serverless(app);
+app.listen(port, () => console.log(`listening on port ${port}`));
