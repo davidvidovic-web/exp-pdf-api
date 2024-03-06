f8251c0fd1a1f3cba3adc4eb170a18c04a29a090
davidvidovic-web <mail@davidvidovic.com>
2024-03-06 10:31:42 +0100

    Implement a try catch block for pdflib

--- server.js
+++ server.js
@@ -2,18 +2,45 @@ const express = require("express");
 const fileupload = require("express-fileupload");
 const port = process.env.PORT || 5030;
 const app = express();
-const { PDFDocument, StandardFonts } = require("pdf-lib");
+const {
+  PDFDocument,
+  StandardFonts,
+  degrees,
+  getRotation,
+  angle,
+} = require("pdf-lib");
 
 app.use(fileupload({ createParentPath: true }));
 
 app.post("/upload", async (req, res, next) => {
   const file = req.files.pdfFile;
   const buffer = file.data;
-  const pdfDoc = await PDFDocument.load(buffer);
+  let pdfDoc;
+  try {
+    pdfDoc = await PDFDocument.load(buffer);
+  } catch (error) {
+    console.error("Error loading PDF document:", error);
+    //return the same PDF file if it's not a valid PDF
+    res.set("Content-Type", "application/pdf");
+    res.send(buffer);
+    return;
+  }
+  const pages = pdfDoc.getPages();
+
+  for (var i = 0; i <= pages.length; i++) {
+    if (pages[i] != undefined) {
+      var orientation = pages[i].getRotation().angle;
+      if (orientation == 90 || orientation == 270) {
+        pages[i].setRotation(degrees(180));
+      }
+    }
+  }
+
   const pdfBytes = await pdfDoc.save({ useObjectStreams: false });
-  console.log('File accepted');
+
   res.set("Content-Type", "application/pdf");
   res.send(Buffer.from(pdfBytes));
+  console.log("File accepted & converted");
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
